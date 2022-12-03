014d13026a81a19335d59c0e5be974572c619458
Vladimir <vova.okhotin@mail.ru>
2022-12-03 18:53:46 +0800

    robot_russian

--- Example_robot/Program.cs
+++ Example_robot/Program.cs
@@ -11,9 +11,8 @@
 Console.WriteLine("Введите кол-во программистов в шт. (целое число)"); // Приглашение
 int col = Convert.ToInt32(Console.ReadLine()); // Ввод и конвертация числа
 int ost = col%10; // Деление для нахождения остатка
-int ost2 = col%100;
+int des = col/10; // находим десятки для исключения чисел от 11 до 19
 string tip = "программист"; // введение строковой переменной, можно обойтись и без этого
-if (ost2>10 && ost2<20){Console.WriteLine($"в комнате {col} {tip}");}
-if (ost==1){Console.WriteLine($"в комнате {col} {tip}");} // вывод просклоненного выражения при определенных условиях
+if (ost==1 && des!=1){Console.WriteLine($"в комнате {col} {tip}");} // вывод просклоненного выражения при определенных условиях
 if (ost>=2 && ost<5){Console.WriteLine($"в комнате {col} {tip}а");}
-if (ost>=5 && ost<9 && ost==0){Console.WriteLine($"в комнате {col} {tip}ов");}
+if (ost>=5 && ost<9 && ost==0 || des==1){Console.WriteLine($"в комнате {col} {tip}ов");}
